1f9e6fc0e293611ea34adf14c6a79ebbf05c67da
amitkishore <amitkishore1234@gmail.com>
2021-09-25 21:34:43 +0530

    Update db.js

--- src/_helpers/db.js
+++ src/_helpers/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGODB_URL = "mongodb://127.0.0.1:27017/ludo-api";
-//const MONGODB_URL =
-// "mongodb+srv://amit1234:Nsit_delhi3@cluster0.d7rgz.mongodb.net/kho-api?retryWrites=true&w=majority";
+//const MONGODB_URL = "mongodb://127.0.0.1:27017/ludo-api";
+const MONGODB_URL =
+  "mongodb+srv://amit1234:Nsit_delhi3@cluster0.d7rgz.mongodb.net/kho-api?retryWrites=true&w=majority";
 
 mongoose.connect(MONGODB_URL, {
   useUnifiedTopology: true,
